0d46973f3a9dfece2b6b0a6e1dec1a2d621bdf89
Lucas28209 <67343109+Lucas28209@users.noreply.github.com>
2022-06-08 11:33:48 -0300

    2.2

--- walking_bot.py
+++ walking_bot.py
@@ -19,7 +19,7 @@ class Ambiente():
         
         a, b = int(self.locais[0,0]), int(self.locais[0,1]) #robo
         
-        self.dados [a,b] = 107 #robo
+        self.dados [a,b] = 1000 #robo
         self.dados [int(self.locais[1,0]), int(self.locais[1,1]) ] = 110 #fábrica 1
         self.dados [int(self.locais[2,0]), int(self.locais[2,1]) ] = 120 #fábrica 2
         self.dados [int(self.locais[3,0]), int(self.locais[3,1]) ] = 230 #fábrica 3
@@ -41,12 +41,40 @@ class Ambiente():
 	#fazer inicio da movimentação
 
 class Robo():
-    def __init__(self,coordenadas):
+    def __init__(self,grid,coordenadas):
         self.raio = 4
         self.coordenadas = coordenadas
+        self.grid = grid
+        self.x = 0
+        self.y = 0
+        
+        self.bracos = 0
+        self.baterias = 0
+        self.bombas = 0
+        self.refrigeracao = 0
+        self.solda = 0
 
-    def andar():
-        pass
+    
+    def andar(self):
+        #grid = self.grid
+        x = self.x + np.random.randint(-1,2) #np.random.randint(-1,2)
+        y = self.y + np.random.randint(-1,2) #np.random.randint(-1,2)
+        return x,y
+        #x,y = self.x, self.y
+        
+        #if grid[x,y] == None:
+         #   if self.carregando:
+          #      self.largar()
+        #elif grid[x,y] != None:
+         #   if not self.carregando:
+          #      self.pegar()
+
+        #self.x, self.y = self.posicao()
+        #self.itera -= 1
+        #print(self.itera)
+    	
+    def run(self):
+    	return self.andar()
 
     def calculos():
         pass
@@ -58,7 +86,7 @@ class Robo():
         pass
 
 
-        pass
+        
 
 class Formiga():
     def __init__(self, x,y,raio_visao, grid,its,alpha, n_dados):
@@ -97,75 +125,7 @@ class Formiga():
         #print(vet[:n,:n])
         return vet[:n,:n]
     
-    def media(self, som_dist):
-        soma = 0
-        if self.carregando:
-            centro = self.data[0:-1]
-        else:
-            centro = self.grid[self.x, self.y][0:-1]
-        
-        for i in range(som_dist.shape[0]):
-            for j in range(som_dist.shape[0]):
-                conta = 0
-                if som_dist[i,j] != None:
-                    conta = 1 - (euclidean(centro, som_dist[i,j][0:-1]))/((self.alpha))
-                soma += conta
-        fi = soma/(self.r_**2)
-        if fi > 0: return fi
-        else: return 0
-        
-        
-    ''' Normalizes the _avg_similarity function '''
-    def _sigmoid(self, c, x):
-        return ((1-np.exp(-(c*x)))/(1+np.exp(-(c*x))))
-    
-    def pegar(self):
-        som_dist = self.vizinhos(self.grid, self.x, self.y, self.r_ )
-
-        f = (self.media(som_dist))
-        #print("f do pegar= ",f)  
-        
-        sig = self._sigmoid(self.raio_visao*10,f)
-        probP = 1 - sig
-        #k1 = 0.1
-        #k1 = 0.05 #este número n pode ser muito grande
-        #probP = (k1/(k1+f))**2   
-
-        #if f <= 1.0:
-        #    probP = 0.9
-        #else:
-        #    probP = (1/f**2) 
-        
-        #print("P=",probP)
-
-        if ((probP)  >= (np.random.uniform(0.0, 1.0))):            
-            self.carregando = True
-            self.data = self.grid[self.x, self.y]
-            self.grid[self.x, self.y] = None
-            return True
-        return False
-
-    def largar(self):
-        som_dist = self.vizinhos(self.grid, self.x, self.y, self.r_ )
-        #print("f do largar= ",f)  
-        f = (self.media(som_dist))
-        probL = self._sigmoid(self.raio_visao*10, f)
-        
-        #k2 = 0.15 #0.15
-        #probL = (f/(k2+f))**2
-        #if f < k2:
-        #    probL = 5*f
-        #else:
-        #    probL = 1
-        
-        #print("L=",probL)
-
-        if (probL >= (np.random.uniform(0.0, 1.0))):            
-            self.carregando = False
-            self.grid[self.x, self.y] = self.data
-            self.data = None
-            return True
-        return False
+      
            
     def run(self):
         self.andar()
@@ -188,10 +148,7 @@ class Formiga():
         self.itera -= 1
         #print(self.itera)
 
-    def _calc_r_(self):
-        self.r_ = 1
-        for i in range(self.raio_visao):
-            self.r_ = self.r_ + 2
+    
 
     def get_carregando(self):
         return self.carregando
@@ -208,44 +165,19 @@ class AntProgram():
         self.locais = locais
         #print(self.locais)
         self.tam = 900
-        self.run()
+        self.cria_robo()
+          
 
-        # self.raio_visao = raio_visao
-        # self.num = num
-        # self.itr = itr
-       
-        # self.dados = dados
-        # self.n_dados = qntd_dados  #1 #criar dados
-        # self.alpha = alpha
-        # #self.n_dados = len(dados_label)
-        # self.lista = list()
-        # self.sleep = sleep
-
-        #self.grid = np.empty((self.size, self.size), dtype=np.object_)
-        #self.distribui(self.grid, self.dados)
-        #print(self.grid)
-        
-        #self.cria_formigas(self.num, self.raio_visao, self.grid, self.itr // self.num, self.alpha, self.n_dados)
-    
-
-    # def distribui(self,grid,dados):
-    #     for a in dados:
-    #         i = np.random.randint(0, self.size)
-    #         j = np.random.randint(0, self.size)
-    #         while grid[i,j] != None:
-    #             i = np.random.randint(0, self.size)
-    #             j = np.random.randint(0, self.size)
-    #         grid[i,j] = a
-    #     #print(self.grid)
-
-      
-
-    # def cria_formigas(self, num, raio_visao, grid, its, alpha, n_dados):
-    #     for i in range(num):
-    #         x = np.random.randint(0,self.size-1)
-    #         y = np.random.randint(0,self.size-1)
-    #         formiga = Formiga(x,y,raio_visao, grid,its, alpha, n_dados)
-    #         self.lista.append(formiga)
+    def cria_robo(self):
+    	robo = Robo(self.ambiente, self.locais)
+    	x,y = robo.run()
+    	self.ambiente[x,y] = 1000
+    	self.run()
+         #for i in range():
+         #    x = np.random.randint(0,self.size-1)
+         #    y = np.random.randint(0,self.size-1)
+             
+             #self.lista.append(formiga)
 
     def inicio(self):
         time.sleep(1)
@@ -312,5 +244,6 @@ if __name__ == "__main__":
     #program = AntProgram(grid=(math.sqrt(10*dados.qntd_dados)), qntd_dados=dados.qntd_dados, dados=dados.dados_labels, alpha=dados.alpha, raio_visao=1, num=20, itr=5*10**6, tam=650,sleep=2)
     #program.run()
    
+
 
 
