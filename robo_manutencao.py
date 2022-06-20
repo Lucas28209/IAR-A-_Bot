07b0511f76a4ad1e6c0e1813d56d7460b4a1324e
Lucas28209 <lucasoliveirarocha147@gmail.com>
2022-06-20 08:46:05 -0300

    só falta o A*

---
+++ robo_manutencao.py
@@ -0,0 +1,197 @@
+import threading
+import numpy as np
+import pygame as pg
+import sys, time
+
+from sklearn.cluster import cluster_optics_dbscan
+
+class Ambiente():
+    def __init__(self):
+        
+        self.locais = np.loadtxt("localizações.txt")
+        self.ambiente = np.loadtxt("ambiente.txt")
+        self.custos = np.zeros((42,42))
+        self.itens = [20,10,8,6,4]
+        
+        #cores 
+        self.green = 25
+        self.blue = 2
+        self.orange = 80
+        self.brown = 40
+
+        for i in range(42):
+            for j in range(42):
+                #print("oi")
+                if self.ambiente[i,j] == 1:
+                    self.ambiente[i,j] = 40 #self.brown
+                    self.custos[i,j] = 5
+                elif self.ambiente[i,j] == 2:
+                    self.ambiente[i,j] = 2 #self.blue
+                    self.custos[i,j] = 10
+                elif self.ambiente[i,j] == 3:
+                    self.ambiente[i,j] =  80 #self.orange
+                    self.custos[i,j] = 15 
+                elif self.ambiente[i,j] == 0:
+                    self.ambiente[i,j] = 25 #self.green  
+                    self.custos[i,j] = 1
+                elif self.ambiente[i,j] == 2000:
+                    self.ambiente[i,j] = 0  #preto, obstáculo
+                    self.custos[i,j] = 2000  
+
+        self.ambiente[int(self.locais[1,0]), int(self.locais[1,1]) ] = 0#110 #fábrica 1
+        self.ambiente[int(self.locais[2,0]), int(self.locais[2,1]) ] = 0#120 #fábrica 2
+        self.ambiente[int(self.locais[3,0]), int(self.locais[3,1]) ] = 0#330 #fábrica 3
+        self.ambiente[int(self.locais[4,0]), int(self.locais[4,1]) ] = 0#140 #fábrica 4   
+
+        for item in self.itens:
+            i=0
+            while i < item:
+                x = np.random.randint(0,42)
+                y = np.random.randint(0,42)
+                if self.ambiente[x,y] == 25:
+                    self.ambiente[x,y] = item
+                    i=i+1
+
+class Robo():
+    def __init__(self,raio, grid,coord,custos):
+        self.grid = grid #ambiente
+        self.raio = raio   
+        self.coord_atual = [coord[0],coord[1]]
+        self.custos = custos
+
+        self.baterias = 0
+        self.solda = 0
+        self.sucçao = 0 
+        self.refri = 0
+        self.pneu = 0
+
+        self.last_coord = [self.coord_atual[0],self.coord_atual[1]] #[x,y]
+        self.last_color = self.grid[self.last_coord[0],self.last_coord[1]] #25
+        
+        self.grid[self.coord_atual[0],self.coord_atual[1]] = 1000 
+        
+        
+
+    def vizinhos(self):
+        pass
+
+    def run(self):
+        self.andar()
+        return self.grid
+    
+    def andar(self):
+        grid = self.grid
+        
+        self.grid[self.coord_atual[0],self.coord_atual[1]] = self.last_color #volta a cor originial
+        
+        self.coord_atual[0] = self.coord_atual[0]+1
+        self.coord_atual[1] = self.coord_atual[1]
+               
+        self.last_color = self.grid[self.coord_atual[0],self.coord_atual[1]]
+
+        if (self.is_item(grid[self.coord_atual[0],self.coord_atual[1]])):
+            self.last_color = 25
+
+        self.grid[self.coord_atual[0],self.coord_atual[1]] = 1000 #muda robo de lugar
+
+        #print(self.custos[self.coord_atual[0],self.coord_atual[1]])
+        
+        return 
+        
+    def is_item(self, item):
+        if (item==20):
+            self.baterias=self.baterias+1
+            print("pegou bateria - total:", self.baterias)
+            return True
+        if (item==10): 
+            self.solda=self.solda+1
+            print("pegou solda - total:", self.solda)
+            return True
+        if (item==8):
+            self.sucçao=self.sucçao+1
+            print("pegou sucçao - total:", self.sucçao)
+            return True
+        if (item==6): 
+            self.refri=self.refri+1
+            print("pegou refri - total:", self.refri)
+            return True
+        if (item==4):
+            self.pneu=self.pneu+1
+            print("pegou pneu - total:", self.pneu)
+            return True
+            
+
+    def pegar(self):
+        pass
+
+ 
+class Acao():
+    def __init__(self,raio,ambiente,coord,custos,sleep):
+        self.size = 42
+        self.raio = raio
+        self.ambiente = ambiente
+        self.coord = [int(coord[0,0]),int(coord[0,1])]
+        self.custos = custos
+        self.sleep = sleep
+        self.pecas = [20,10,8,6,4] #peças
+        self.lista = list()
+
+        #print(self.ambiente)
+
+        #self.distribui(self.ambiente, self.pecas)
+        self.cria_robo(self.raio, self.ambiente,self.coord,self.custos)
+
+       
+    
+    def distribui(self, ambiente, pecas):
+        #solta as peças no ambiente
+        pass
+
+    def cria_robo(self,raio,ambiente,coord,custos):
+        robo = Robo(raio, ambiente,coord,custos)
+        self.lista.append(robo)
+    
+    def inicio(self):
+        #time.sleep(self.sleep)
+        for robo in self.lista:
+            for i in range (0,10):
+                self.ambiente = robo.run()                   
+                self.run()
+
+    def matriz(self):
+        ret = np.zeros((self.size, self.size))
+        for i in range(self.size):
+            for j in range(self.size):
+                ret[i,j] = self.ambiente[i,j]
+        #print(ret)
+        return ret
+              
+
+    def run(self):
+            time.sleep(0.5) 
+            pg.init()
+            tela = pg.display.set_mode((900, 900))
+            tela.set_alpha(None)
+            #print(np.count_nonzero(self.grid != None))
+            #t = threading.Thread(target=self.inicio)
+            #t.daemon=True
+            #t.start()
+
+            #while True:
+            for e in pg.event.get():
+                if e.type == pg.QUIT: sys.exit()
+            vetor = self.matriz() #self.matriz()
+            superficie = pg.surfarray.make_surface(vetor)
+            new_superficie = pg.transform.scale(superficie, (900,900))
+                
+            new_superficie = pg.transform.flip(new_superficie, True, False)  
+            new_superficie = pg.transform.rotate(new_superficie,90)
+                
+            tela.blit(new_superficie, (0,0))
+            pg.display.flip()
+
+if __name__ == "__main__":
+    ambiente = Ambiente()
+    start = Acao(raio=4,ambiente=ambiente.ambiente,coord=ambiente.locais,custos=ambiente.custos,sleep=1)
+    start.inicio()
+    
--- walking_bot.py
+++ walking_bot.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-  
-from cProfile import run
+   
 import threading
-#from matplotlib.pyplot import grid
+from matplotlib.pyplot import grid
 import numpy as np
 import time
 import pygame as pg
@@ -9,99 +8,42 @@ import sys
 from scipy.spatial.distance import euclidean
 import math
 
-''''''
-class Ambiente():
-    def __init__(self, diretorio, diretorio2):
-        self.dados = np.loadtxt(diretorio) #leitura do ambiente
-        self.locais = np.loadtxt(diretorio2) #leitura das posições das fábricas e robô  
-        
-        self.itens = [20,10,8,6,4] #itens
-        
-        a, b = int(self.locais[0,0]), int(self.locais[0,1]) #robo
-        
-        self.dados [a,b] = 1000 #robo
-        self.dados [int(self.locais[1,0]), int(self.locais[1,1]) ] = 110 #fábrica 1
-        self.dados [int(self.locais[2,0]), int(self.locais[2,1]) ] = 120 #fábrica 2
-        self.dados [int(self.locais[3,0]), int(self.locais[3,1]) ] = 230 #fábrica 3
-        self.dados [int(self.locais[4,0]), int(self.locais[4,1]) ] = 140 #fábrica 4
-        
-        
-        for i in range (len(self.itens)):
-            j=0
-            while j < self.itens[i]:
-                a = np.random.randint(0,high=42)
-                b = np.random.randint(0,high=42)
-                if (self.dados[a,b] == 0):
-                    self.dados[a,b] = self.itens[i]*25
-                    j=j+1	
+class Dados():
+    def __init__(self):
+        self.locais = np.loadtxt("localizações.txt")
+        self.ambiente = np.loadtxt("ambiente.txt")
 
-        #se pegar, colocar custo 1(verde) 
-	#criar classe robô
-	#fazer o raio
-	#fazer inicio da movimentação
+        for i in range(42):
+            for j in range(42):
+                #print("oi")
+                if self.ambiente[i,j] == 1:
+                    self.ambiente[i,j] = 40 #self.brown
+                elif self.ambiente[i,j] == 2:
+                    self.ambiente[i,j] = 2 #self.blue
+                elif self.ambiente[i,j] == 3:
+                    self.ambiente[i,j] =  80 #self.orange 
+                elif self.ambiente[i,j] == 0:
+                    self.ambiente[i,j] = 25 #self.green      
 
-class Robo():
-    def __init__(self,grid,coordenadas):
-        self.raio = 4
-        self.coordenadas = coordenadas
-        self.grid = grid
-        self.x = 0
-        self.y = 0
-        
-        self.bracos = 0
-        self.baterias = 0
-        self.bombas = 0
-        self.refrigeracao = 0
-        self.solda = 0
-
-    
-    def andar(self):
-        #grid = self.grid
-        x = self.x + np.random.randint(-1,2) #np.random.randint(-1,2)
-        y = self.y + np.random.randint(-1,2) #np.random.randint(-1,2)
-        return x,y
-        #x,y = self.x, self.y
-        
-        #if grid[x,y] == None:
-         #   if self.carregando:
-          #      self.largar()
-        #elif grid[x,y] != None:
-         #   if not self.carregando:
-          #      self.pegar()
-
-        #self.x, self.y = self.posicao()
-        #self.itera -= 1
-        #print(self.itera)
-    	
-    def run(self):
-    	return self.andar()
-
-    def calculos():
-        pass
-
-    def pegar():
-        pass
-
-    def largar():
-        pass
-
-
+        self.ambiente[int(self.locais[1,0]), int(self.locais[1,1]) ] = 110 #fábrica 1
+        self.ambiente[int(self.locais[2,0]), int(self.locais[2,1]) ] = 120 #fábrica 2
+        self.ambiente[int(self.locais[3,0]), int(self.locais[3,1]) ] = 330 #fábrica 3
+        self.ambiente[int(self.locais[4,0]), int(self.locais[4,1]) ] = 140 #fábrica 4  
         
 
 class Formiga():
-    def __init__(self, x,y,raio_visao, grid,its,alpha, n_dados):
+    def __init__(self, x,y,raio_visao, grid,its):
         self.grid = grid
         self.raio_visao = raio_visao
         self.x = x
         self.y = y
         self.itera = its
-        self._calc_r_()
+        
         self.carregando = False
         self.data = None
-        #self.c              = self.raio_visao*10
+        
         self.max_step_size  = self.grid.shape[0]//2 +1 #int((20*n_dados)**0.5)
-        #print(self.max_step_size)
-        self.alpha = alpha
+
 
     def posicao(self):
         #print("passo",self.max_step_size, self.grid.shape[0])
@@ -125,7 +67,58 @@ class Formiga():
         #print(vet[:n,:n])
         return vet[:n,:n]
     
-      
+    def media(self, som_dist):
+        soma = 0
+        if self.carregando:
+            centro = self.data[0:-1]
+        else:
+            centro = self.grid[self.x, self.y][0:-1]
+        
+        for i in range(som_dist.shape[0]):
+            for j in range(som_dist.shape[0]):
+                conta = 0
+                if som_dist[i,j] != None:
+                    conta = 1 - (euclidean(centro, som_dist[i,j][0:-1]))/((self.alpha))
+                soma += conta
+        fi = soma/(self.r_**2)
+        if fi > 0: return fi
+        else: return 0
+        
+        
+    ''' Normalizes the _avg_similarity function '''
+    def _sigmoid(self, c, x):
+        return ((1-np.exp(-(c*x)))/(1+np.exp(-(c*x))))
+    
+    def pegar(self):
+        som_dist = self.vizinhos(self.grid, self.x, self.y, self.r_ )
+
+        f = (self.media(som_dist))
+        #print("f do pegar= ",f)  
+        
+        sig = self._sigmoid(self.raio_visao*10,f)
+        probP = 1 - sig
+  
+
+        if ((probP)  >= (np.random.uniform(0.0, 1.0))):            
+            self.carregando = True
+            self.data = self.grid[self.x, self.y]
+            self.grid[self.x, self.y] = None
+            return True
+        return False
+
+    def largar(self):
+        som_dist = self.vizinhos(self.grid, self.x, self.y, self.r_ )
+        #print("f do largar= ",f)  
+        f = (self.media(som_dist))
+        probL = self._sigmoid(self.raio_visao*10, f)
+        
+
+        if (probL >= (np.random.uniform(0.0, 1.0))):            
+            self.carregando = False
+            self.grid[self.x, self.y] = self.data
+            self.data = None
+            return True
+        return False
            
     def run(self):
         self.andar()
@@ -146,9 +139,7 @@ class Formiga():
 
         self.x, self.y = self.posicao()
         self.itera -= 1
-        #print(self.itera)
 
-    
 
     def get_carregando(self):
         return self.carregando
@@ -159,64 +150,71 @@ class Formiga():
         
 
 class AntProgram():
-    def __init__(self, ambiente, locais):
-        self.size = 42
-        self.ambiente = ambiente
-        self.locais = locais
-        #print(self.locais)
-        self.tam = 900
-        self.cria_robo()
-          
+    def __init__(self, grid, dados, raio_visao, num, itr, tam,sleep=0):
+        self.size = int(grid)
+        self.raio_visao = raio_visao
+        self.num = num
+        self.itr = itr
+        self.tam = tam
+        self.dados = dados
+        
+        self.lista = list()
+        self.sleep = sleep
 
-    def cria_robo(self):
-    	robo = Robo(self.ambiente, self.locais)
-    	x,y = robo.run()
-    	self.ambiente[x,y] = 1000
-    	self.run()
-         #for i in range():
-         #    x = np.random.randint(0,self.size-1)
-         #    y = np.random.randint(0,self.size-1)
-             
-             #self.lista.append(formiga)
+        self.grid = np.empty((self.size, self.size), dtype=np.object_)
+        self.distribui(self.grid, self.dados)
+     
+        
+        self.cria_formigas(1, self.raio_visao, self.grid, self.itr // self.num)
+    
+
+    def distribui(self,grid,dados):
+        grid = self.dados
+        # for a in dados:
+        #     i = np.random.randint(0, self.size)
+        #     j = np.random.randint(0, self.size)
+        #     while grid[i,j] != None:
+        #         i = np.random.randint(0, self.size)
+        #         j = np.random.randint(0, self.size)
+        #     grid[i,j] = a
+        #print(self.grid)
+
+      
+
+    def cria_formigas(self, num, raio_visao, grid, its):
+        for i in range(num):
+            x = np.random.randint(0,self.size-1)
+            y = np.random.randint(0,self.size-1)
+            formiga = Formiga(x,y,raio_visao, grid,its)
+            self.lista.append(formiga)
 
     def inicio(self):
-        time.sleep(1)
-        
-        # for i in range(self.itr // self.num):
-        #     for formiga in self.lista:
-        #         formiga.run()
-        # l = list()
-        # for formiga in self.lista:
-        #     l.append(formiga.get_carregando())
-        # print(l)
+        time.sleep(self.sleep)
+        for i in range(self.itr // self.num):
+            for formiga in self.lista:
+                formiga.run()
+        l = list()
+        for formiga in self.lista:
+            l.append(formiga.get_carregando())
+        print(l)
 
     def matriz(self):
         ret = np.zeros((self.size, self.size))
-        #a, b = int(self.locais[0,0]), int(self.locais[0,1]) #robo
-        
-        #faz a coloração do ambiente
         for i in range(self.size):
             for j in range(self.size):
                 #print("oi")
-                if self.ambiente[i,j] == 1:
-                    #data = self.grid[i,j]
-                    ret[i,j] = 40#self.brown
-                elif self.ambiente[i,j] == 2:
-                    ret[i,j] = 2#self.blue
-                elif self.ambiente[i,j] == 3:
-                    ret[i,j] =  80#self.orange 
-                elif self.ambiente[i,j] == 0:
-                    ret[i,j] = 25#self.green                   
+                if self.grid[i,j] != None:
+                    #print("oi")
+                    data = self.grid[i,j]
+                    ret[i,j] = data[-1] #50 #cor dos dados
                 else:
-                    ret[i,j] = self.ambiente[i,j]                                     
-        
-        
+                    ret[i,j] = self.grid[i,j]
         return ret
         
 
     def run(self):
         pg.init()
-        tela = pg.display.set_mode((900, 900))
+        tela = pg.display.set_mode((self.tam, self.tam))
         tela.set_alpha(None)
         #print(np.count_nonzero(self.grid != None))
         t = threading.Thread(target=self.inicio)
@@ -229,21 +227,14 @@ class AntProgram():
             vetor = self.matriz()
             formigueiro = pg.surfarray.make_surface(vetor)
             n_formigueiro = pg.transform.scale(formigueiro, (self.tam, self.tam))
-            
-            n_formigueiro = pg.transform.flip(n_formigueiro, True, False)  
-            n_formigueiro = pg.transform.rotate(n_formigueiro,90)
-            
             tela.blit(n_formigueiro, (0,0))
             pg.display.flip()
 
 
 if __name__ == "__main__":
-    dados = Ambiente('ambiente.txt', 'localizações.txt')
-    AntProgram(ambiente=dados.dados, locais=dados.locais)
-   
-    #program = AntProgram(grid=(math.sqrt(10*dados.qntd_dados)), qntd_dados=dados.qntd_dados, dados=dados.dados_labels, alpha=dados.alpha, raio_visao=1, num=20, itr=5*10**6, tam=650,sleep=2)
-    #program.run()
-   
-
+    dados = Dados()
+    
+    program = AntProgram(grid=42, dados=dados.ambiente, raio_visao=4, num=20, itr=5*10**6, tam=900,sleep=2)
+    program.run()
 
 
